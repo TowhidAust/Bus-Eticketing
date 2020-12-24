0d9ccb8b4eea8936c1f96ae2f42e11b9c55cfca8
TowhidAust <007towhid2016@gmail.com>
2020-12-24 16:36:16 +0600

    organized

--- bus_eicketing/src/App.js
+++ bus_eicketing/src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import "./App.css"
 
-export default class App extends Component {
+export default class Home extends Component {
   render() {
     return (
-      <div>
-        create react App
-      </div>
+      <Router>
+        <div className="MainAppContainer"> 
+          <Switch>
+            {/* <Route path="/" exact component={Home}></Route> */}
+            <div>akjshf </div>
+              
+          </Switch>
+        </div>
+          
+      </Router>
     )
   }
 }
